d16e990f6231aaae374f8404d066f077b730c995
Sharon Watkins <swatkins876@gmail.com>
2019-10-09 18:36:43 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
   
   def back_project(project) 
     @backed_projects << project 
-    @backers << Backer 
+    project.add_backer(self) unless project.backers.include?(self)
   end 
   
 end 
