281c5e1cd1d1a6c2a42d7d3aa3521794bd020dc8
Sharon Watkins <swatkins876@gmail.com>
2019-10-09 18:38:02 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+  
+  attr_reader :title
+  attr_accessor :backers
+  
+  def initialize(title)
+    @title = title 
+    @backers = []
+  end 
+  
+  def add_backer(backer)
+    @backers << backer 
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+  
+end 
